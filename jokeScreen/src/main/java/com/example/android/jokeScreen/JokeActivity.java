917b2420ddd02c8a275d255b1c4a03740fc88ac8
David Darrell <david.darrell@gmail.com>
2015-09-26 14:47:17 +0800

    Add an Android Library

--- jokeScreen/src/main/java/com/example/android/jokeScreen/JokeActivity.java
+++ jokeScreen/src/main/java/com/example/android/jokeScreen/JokeActivity.java
@@ -1,30 +1,25 @@
-package com.udacity.gradle.builditbigger;
+package com.example.android.jokeScreen;
 
-import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
 
-import com.example.Jokes;
-import com.example.android.jokeScreen.JokeActivity;
+public class JokeActivity extends AppCompatActivity {
 
-
-public class MainActivity extends ActionBarActivity {
+    public static String JOKE_KEY = "Joke key";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_joke);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_joke, menu);
         return true;
     }
 
@@ -42,14 +37,4 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-    public void tellJoke(View view){
-        // Get a joke and display it in a new activity.
-        Jokes jokes = new Jokes();
-        Intent intent = new Intent(this, JokeActivity.class);
-        intent.putExtra(JokeActivity.JOKE_KEY, jokes.tellJoke());
-        startActivity(intent);
-    }
-
-
 }
