011a54cd24b3a84bf86d5ff3d5110a57a7525342
David Darrell <david.darrell@gmail.com>
2015-09-26 12:05:05 +0800

    Add a Java library

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.Jokes;
+
 
 public class MainActivity extends ActionBarActivity {
 
@@ -40,7 +42,8 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void tellJoke(View view){
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        Jokes jokes = new Jokes();
+        Toast.makeText(this, jokes.tellJoke(), Toast.LENGTH_SHORT).show();
     }
 
 
---
+++ libjokes/src/main/java/com/example/Jokes.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class Jokes {
+    public String tellJoke(){
+        return "This is funny library joke";
+    }
+}
